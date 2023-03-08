d8c7e1ba2231c924fec4e02168109c1d83aa9f21
Leonidas Ramirez <leonidas.ramirez.q@gmail.com>
2023-03-07 22:52:52 -0500

    clase 08 express y multer

--- modulo1/clase-8-router-multer/hands-on-lab/express-router/src/app.js
+++ modulo1/clase-8-router-multer/hands-on-lab/express-router/src/app.js
@@ -1,15 +1,17 @@
 import express from "express";
 import usersRouter from "./routes/users.router.js";
 import petsRouter from "./routes/pets.router.js";
+import __dirname from "./utils.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static("public"));
-
-app.listen(8080, () => {
-  console.log("Listening on port 8080");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use("/", express.static(`${__dirname}/public`));
 
 app.use("/api/pets", petsRouter);
 app.use("/api/users", usersRouter);
+
+const server = app.listen(8080, () => {
+  console.log("Listening on port 8080");
+});
