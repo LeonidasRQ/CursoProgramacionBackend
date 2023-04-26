f2b27c00f5d0473447966b176497d523e0208f4c
Leonidas Ramirez <leonidas.ramirez.q@gmail.com>
2023-04-25 23:39:03 -0500

    basic permissions middleware

--- modulo-02/22-passport-avanzado/src/app.js
+++ modulo-02/22-passport-avanzado/src/app.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 import initializePassport from "./passport.js";
 import passport from "passport";
-import { passportCall } from "./utils.js";
+import { passportCall, authorization } from "./utils.js";
 
 const app = express();
 
@@ -17,14 +17,16 @@ app.post("/login", (req, res) => {
   const { email, password } = req.body;
 
   if (email === "leo@correo" && password === "123") {
-    let token = jwt.sign({ email, password }, "coderKey", { expiresIn: "24h" });
+    let token = jwt.sign({ email, password, role: "user" }, "coderKey", {
+      expiresIn: "24h",
+    });
     return res
       .cookie("tokenCookie", token, { httpOnly: true })
       .send({ status: "sucess", token });
   }
 });
 
-app.get("/current", passportCall("jwt"), (req, res) => {
+app.get("/current", passportCall("jwt"), authorization("user"), (req, res) => {
   res.send(req.user);
 });
 
--- modulo-02/22-passport-avanzado/src/utils.js
+++ modulo-02/22-passport-avanzado/src/utils.js
@@ -15,3 +15,14 @@ export const passportCall = (strategy) => {
     })(req, res, next);
   };
 };
+
+export const authorization = (role) => {
+  return async (req, res, next) => {
+    if (!req.user) return res.status(401).send({ error: "Unauthorized" });
+
+    if (req.user.role != role)
+      return res.status(403).send({ error: "No permissions" });
+
+    next();
+  };
+};
