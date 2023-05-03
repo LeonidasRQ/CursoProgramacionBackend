fbeb0709512dd34a53b9c312dfe46addd55ddd0f
Leonidas Ramirez <leonidas.ramirez.q@gmail.com>
2023-05-03 16:01:59 -0500

    clase 23 custom responses

--- modulo-02/23-ruteo-avanzado-manejo-politicas/ejemplos/custom-router/src/routes/router.js
+++ modulo-02/23-ruteo-avanzado-manejo-politicas/ejemplos/custom-router/src/routes/router.js
@@ -13,7 +13,35 @@ export default class CustomRouter {
   init() {}
 
   get(path, ...callbacks) {
-    this.router.get(path, this.applyCallbacks(callbacks));
+    this.router.get(
+      path,
+      this.generateCustomResponses,
+      this.applyCallbacks(callbacks)
+    );
+  }
+
+  post(path, ...callbacks) {
+    this.router.post(
+      path,
+      this.generateCustomResponses,
+      this.applyCallbacks(callbacks)
+    );
+  }
+
+  put(path, ...callbacks) {
+    this.router.put(
+      path,
+      this.generateCustomResponses,
+      this.applyCallbacks(callbacks)
+    );
+  }
+
+  delete(path, ...callbacks) {
+    this.router.delete(
+      path,
+      this.generateCustomResponses,
+      this.applyCallbacks(callbacks)
+    );
   }
 
   applyCallbacks(callbacks) {
@@ -26,5 +54,17 @@ export default class CustomRouter {
         params[1].status(500).send(error);
       }
     });
+  }
+
+  generateCustomResponses(req, res, next) {
+    res.sendSucess = (payload) => res.send({ status: "sucess", payload });
+
+    res.sendServerError = (error) =>
+      res.status(500).send({ status: "error", error });
+
+    res.sendUserError = (error) =>
+      res.status(400).send({ status: "error", error });
+
+    next();
   }
 }
--- modulo-02/23-ruteo-avanzado-manejo-politicas/ejemplos/custom-router/src/routes/users.js
+++ modulo-02/23-ruteo-avanzado-manejo-politicas/ejemplos/custom-router/src/routes/users.js
@@ -3,7 +3,7 @@ import CustomRouter from "./router.js";
 export default class UsersRouter extends CustomRouter {
   init() {
     this.get("/", (req, res) => {
-      res.send("Hola, Coders");
+      res.sendSucess("Hola, coders");
     });
   }
 }
