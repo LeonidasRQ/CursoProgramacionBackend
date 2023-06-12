810d976884063bb13cb221907cd6dc0c8810957c
Leonidas Ramirez <leonidas.ramirez.q@gmail.com>
2023-06-12 16:40:31 -0500

    ejemplo: artillery-sencillo

--- modulo-03/34-logging-performance-testing/ejemplos/src/app.js
+++ modulo-03/34-logging-performance-testing/ejemplos/src/app.js
@@ -11,6 +11,22 @@ app.get("/", (req, res) => {
   res.send({ message: "Prueba de Logger" });
 });
 
+app.get("/operacionSencilla", (req, res) => {
+  let sum = 0;
+  for (let i = 0; i < 100000; i++) {
+    sum += i;
+  }
+  res.send({ sum });
+});
+
+app.get("/operacionCompleja", (req, res) => {
+  let sum = 0;
+  for (let i = 0; i < 5e8; i++) {
+    sum += i;
+  }
+  res.send({ sum });
+});
+
 app.listen(8080, () => {
   console.log("Listening on port 8080");
 });
